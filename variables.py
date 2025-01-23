edfbc1f961b2067fff6257579b61c562cfce460d
cherrydwen <robin.fresh07@gmail.com>
2025-01-23 15:58:38 +0300

    homework

---
+++ variables.py
@@ -0,0 +1,6 @@
+quant_hw = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_per_task = hours_spent/quant_hw
+
+print('Курс:', course_name,',', 'всего задач:', quant_hw,',', 'затрачено часов:', hours_spent,',', 'среднее время выполнения:', time_per_task)

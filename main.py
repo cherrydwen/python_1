0409867a9d58f9f7e48cae7a57891db47cdc370f
cherrydwen <robin.fresh07@gmail.com>
2025-01-23 14:59:27 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print('task 1') # 1st program
+print(float((9**0.5)*5))
+
+print('task 2') # 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+
+print('task 3') # 3d program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+
+print('task 4') # 4th program
+print((int((float('123.456'))*10))%10)
